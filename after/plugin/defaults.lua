a6047790dcb73bd03d45c3907afc6fe06ea9618e
Slawek Zachcial <slawek.zachcial@hp.com>
2023-04-16 17:12:50 +0200

    Update detault settings

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,17 @@
-vim.opt.colorcolumn = "80"
-vim.opt.cursorline = true
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = '80'
+vim.opt.cursorline = true
+vim.opt.swapfile = false
+vim.opt.list = true
+vim.opt.listchars = 'tab:→ ,trail:·,precedes:«,extends:»'
+vim.opt.spelllang = 'en_us'
+
+-- Cycle between buffers
+vim.keymap.set('n', '<C-b>', ':edit #<cr>')
+
+-- Terraform - format on save
+require'lspconfig'.terraformls.setup{}
+vim.api.nvim_create_autocmd({'BufWritePre'}, {
+  pattern = {'*.tf', '*.tfvars'},
+  callback = function() vim.lsp.buf.format() end,
+})

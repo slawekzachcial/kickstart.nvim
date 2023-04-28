58fde18b6d5cc3cf8b6880bfe5c6b06e2eded668
Slawek Zachcial <slawek.zachcial@hp.com>
2023-04-28 19:30:22 +0200

    Various changes

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -7,10 +7,12 @@ vim.opt.listchars = 'tab:→ ,trail:·,precedes:«,extends:»'
 vim.opt.spelllang = 'en_us'
 
 -- Cycle between buffers
-vim.keymap.set('n', '<C-b>', ':edit #<cr>')
+vim.keymap.set('n', '<C-b>', ':edit #<cr>', { desc = 'Switch to previous buffer' })
 
 -- Terraform - format on save
-require'lspconfig'.terraformls.setup{}
+require'lspconfig'.terraformls.setup{
+  root_dir = require('lspconfig.util').root_pattern('.git', '.terraform')
+}
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
   pattern = {'*.tf', '*.tfvars'},
   callback = function() vim.lsp.buf.format() end,

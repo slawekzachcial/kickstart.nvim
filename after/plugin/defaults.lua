3b2be6a23c66b78bcd96efc8db121a912c0b30ae
Slawek Zachcial <slawek.zachcial@hp.com>
2023-05-16 15:50:27 +0200

    Add toggle keymaps

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -9,10 +9,15 @@ vim.opt.spelllang = 'en_us'
 -- Cycle between buffers
 vim.keymap.set('n', '<C-b>', ':edit #<cr>', { desc = 'Switch to previous buffer' })
 
+-- Toggle things
+vim.keymap.set('n', '<leader>th', ':set hlsearch!<cr>', { desc = '[T]oggle search [h]ighlight' })
+vim.keymap.set('n', '<leader>ts', ':set spell!<cr>', { desc = '[T]oggle [s]pelling' })
+
 -- Terraform - format on save
 require'lspconfig'.terraformls.setup{
   root_dir = require('lspconfig.util').root_pattern('.git', '.terraform')
 }
+
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
   pattern = {'*.tf', '*.tfvars'},
   callback = function() vim.lsp.buf.format() end,
